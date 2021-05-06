5259a36517a4f84745b16548b80bf0752db64d50
Si yeon Park <xion2664@gmail.com>
2021-05-06 16:52:14 +0900

    Add files via upload

---
+++ Chapter02/69p_error2.c
@@ -0,0 +1,9 @@
+/*주석 실수* /
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!");
+	return 0;
+}
---
+++ Chapter02/70p_error4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	print("Hey!");
+	print("Good Morning!");
+	return 0;
+}
