b2eccf56b09792eeb6cb15c3e8d02b2db94ebc0e
Si yeon Park <xion2664@gmail.com>
2021-05-07 14:03:53 +0900

    Delete Chapter04 directory

--- Chapter04/Programming/152p_08.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	printf("%c %c %c\n", 'a' + 1, 'a' + 2, 'a' + 3);
-}
--- Chapter04/Programming/152p_09.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("\a");
-	printf("화재가 발생하였습니다!!!\n");
-	printf("\a");
-}
